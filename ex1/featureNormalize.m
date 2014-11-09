a0db851e5092854315788787f052b9248612bc07
rishabh <rishabhsixfeet@gmail.com>
2014-11-10 00:58:00 +0530

    final

--- ex1/featureNormalize.m
+++ ex1/featureNormalize.m
@@ -4,29 +4,38 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   the mean value of each feature is 0 and the standard deviation
 %   is 1. This is often a good preprocessing step to do when
 %   working with learning algorithms.
- 
-% Initialize some useful values
+
+% You need to set these values correctly
 X_norm = X;
 mu = zeros(1, size(X, 2));
 sigma = zeros(1, size(X, 2));
- 
-% For each feature dimension, compute the mean
-% of the feature and subtract it from the dataset,
-% storing the mean value in mu. Next, compute the 
-% standard deviation of each feature and divide
-% each feature by it's standard deviation, storing
-% the standard deviation in sigma. 
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: First, for each feature dimension, compute the mean
+%               of the feature and subtract it from the dataset,
+%               storing the mean value in mu. Next, compute the 
+%               standard deviation of each feature and divide
+%               each feature by it's standard deviation, storing
+%               the standard deviation in sigma. 
 %
-% Note that X is a matrix where each column is a 
-% feature and each row is an example. We need 
-% to perform the normalization separately for 
-% each feature. 
- 
-	mu = sum(X) / length(X);
-	sigma = std(X);
-	
-	for i=1:length(X),
-		X_norm(i,:) = (X(i,:) - mu) ./ sigma;
-	end
- 
+%               Note that X is a matrix where each column is a 
+%               feature and each row is an example. You need 
+%               to perform the normalization separately for 
+%               each feature. 
+%
+% Hint: You might find the 'mean' and 'std' functions useful.
+%       
+
+
+mu = mean(X);
+sigma = std(X);
+X_norm = (X - repmat(mu, [size(X,1), 1]))./repmat(sigma, [size(X,1), 1]);
+
+
+
+
+
+
+% ============================================================
+
 end
